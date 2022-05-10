21396d979899d0cb0a656dcb62d74ba31069e7aa
facundo2309 <facundoomargarcia@gmail.com>
2022-05-10 14:53:40 -0300

    ruta post para el join de los videos

--- src/service/routes/flowvideo.routes.ts
+++ src/service/routes/flowvideo.routes.ts
@@ -1,14 +1,29 @@
 import {Router} from 'restify-router';
 import {videoFlowController} from '../controllers/videoflow.controller';
+import {tasckFlowController} from '../controllers/taskflow.controller';
 
 const routerFlowInstance = new Router();
-
 
 routerFlowInstance.post('/processvideo', async (req, res) => {
   try {
     const {inputvideo} = req.body;
     res.json({inputvideo});
     await videoFlowController.executeProcess(inputvideo);
+  } catch (error) {
+    let errorMessage = 'Failed to do something exceptional';
+    if (error instanceof Error) {
+      errorMessage = error.message;
+    }
+    return res.json({success: false, error: true, message: errorMessage});
+  }
+});
+
+routerFlowInstance.post('/processvideoJoin', async (req, res) => {
+  try {
+    const {pathVideoI,pathVideoII,pathVideoIII,pathVideoIV} = req.body;
+
+    let response=await tasckFlowController.executeProcess(pathVideoI,pathVideoII,pathVideoIII,pathVideoIV)
+    res.json({response})
   } catch (error) {
     let errorMessage = 'Failed to do something exceptional';
     if (error instanceof Error) {
